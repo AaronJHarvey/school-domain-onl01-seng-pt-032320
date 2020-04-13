5a753616f7cb9c322bd39cff07fbd6a182fa9956
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-13 23:54:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  attr_reader :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+  end
+
+def add_student(name, year)
+ @roster[year] = []
+ @roster[year] = [name]
+end
+
+
+end
